86d7a860f5e7a8fa9f6c5dc83e76d8bb2c5d923b
Kaizen-Mage <simulationworld4@gmail.com>
2026-07-09 12:31:47 +0300

    Don with assignment

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,7 +1,17 @@
 // Write your code here
-
-
-
+let products=["Laptop","Phone","HeadPhones","Monitor"];
+function logFirstProduct(){
+    console.log(products[0]);
+}
+function addProduct(product){
+  products.push(product);
+}
+function updateProductName(position,newName){
+  products[position]=newName;
+}
+function removeLastProduct(){
+  products.pop()
+}
 // Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
